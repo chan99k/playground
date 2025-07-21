21af752ebdc20073a1deda88a1d61cb88970a6aa
chan99k <kjkj5868@gmail.com>
2025-07-21 21:42:49 +0900

    eff-java : 빌더 패턴 강화

--- effective-java/src/main/java/chan99k/builder/step02/Person.java
+++ effective-java/src/main/java/chan99k/builder/step02/Person.java
@@ -6,14 +6,14 @@ import java.time.LocalDate;
  * 여러가지 종류의 생성자를 용도에 맞게 매번 작성하는 것에는 무리가 있다.
  */
 public class Person {
-	private String firstName;
-	private String lastName;
-	private LocalDate birthDate;
-	private String addressOne;
-	private String addressTwo;
-	private String sex;
-	private boolean driverLicence;
-	private boolean married;
+	private final String firstName;
+	private final String lastName;
+	private final LocalDate birthDate;
+	private final String addressOne;
+	private final String addressTwo;
+	private final String sex;
+	private final boolean driverLicence;
+	private final boolean married;
 
 	@Override
 	public String toString() {
@@ -29,71 +29,80 @@ public class Person {
 			'}';
 	}
 
-	public String getFirstName() {
-		return firstName;
+	// Builder를 통해서만 생성할 수 있도록 private 생성자를 정의한다.
+	private Person(PersonBuilder builder) {
+		this.firstName = builder.firstName;
+		this.lastName = builder.lastName;
+		this.birthDate = builder.birthDate;
+		this.addressOne = builder.addressOne;
+		this.addressTwo = builder.addressTwo;
+		this.sex = builder.sex;
+		this.driverLicence = builder.driverLicence;
+		this.married = builder.married;
 	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public LocalDate getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(LocalDate birthDate) {
-		this.birthDate = birthDate;
-	}
-
-	public String getAddressOne() {
-		return addressOne;
-	}
-
-	public void setAddressOne(String addressOne) {
-		this.addressOne = addressOne;
-	}
-
-	public String getAddressTwo() {
-		return addressTwo;
-	}
-
-	public void setAddressTwo(String addressTwo) {
-		this.addressTwo = addressTwo;
-	}
-
-	public String getSex() {
-		return sex;
-	}
-
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-
-	public boolean isDriverLicence() {
-		return driverLicence;
-	}
-
-	public void setDriverLicence(boolean driverLicence) {
-		this.driverLicence = driverLicence;
-	}
-
-	public boolean isMarried() {
-		return married;
-	}
-
-	public void setMarried(boolean married) {
-		this.married = married;
-	}
-
+	// 외부에서 빌더를 생성할 수 있는 static 팩토리 메서드
 	public static PersonBuilder builder() {
 		return new PersonBuilder();
+	}
+
+	// static 내부 클래스로 Builder를 정의한다.
+	public static class PersonBuilder {
+		private String firstName;
+		private String lastName;
+		private LocalDate birthDate;
+		private String addressOne;
+		private String addressTwo;
+		private String sex;
+		private boolean driverLicence;
+		private boolean married;
+
+		// 필수값이 있다면 빌더의 생성자에서 받도록 설계할 수 있다.
+		// public PersonBuilder(String firstName, String lastName) { ... }
+
+		public PersonBuilder firstName(String firstName) {
+			this.firstName = firstName;
+			return this;
+		}
+
+		public PersonBuilder lastName(String lastName) {
+			this.lastName = lastName;
+			return this;
+		}
+
+		public PersonBuilder birthDate(LocalDate birthDate) {
+			this.birthDate = birthDate;
+			return this;
+		}
+
+		public PersonBuilder addressOne(String addressOne) {
+			this.addressOne = addressOne;
+			return this;
+		}
+
+		public PersonBuilder addressTwo(String addressTwo) {
+			this.addressTwo = addressTwo;
+			return this;
+		}
+
+		public PersonBuilder sex(String sex) {
+			this.sex = sex;
+			return this;
+		}
+
+		public PersonBuilder driverLicence(boolean driverLicence) {
+			this.driverLicence = driverLicence;
+			return this;
+		}
+
+		public PersonBuilder married(boolean married) {
+			this.married = married;
+			return this;
+		}
+
+		// private 생성자를 호출하여 최종 불변 객체를 생성한다.
+		public Person build() {
+			return new Person(this);
+		}
 	}
 }
