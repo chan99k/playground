26770e2d0f3a3ad8774b8c0b349a81b2e47b1e85
chan99k <kjkj5868@gmail.com>
2025-09-01 22:52:07 +0900

    algorithm: Leet_1774 Closest Dessert Cost 풀이

---
+++ algorithm/src/main/java/chan99k/Leet_1774.java
@@ -0,0 +1,38 @@
+package chan99k;
+
+public class Leet_1774 {
+	private int closestCost = Integer.MAX_VALUE;
+	private int minDiff = Integer.MAX_VALUE;
+
+	public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
+
+		for (int baseCost : baseCosts) {
+			dfs(toppingCosts, 0, baseCost, target);
+		}
+		return closestCost;
+	}
+
+	private void dfs(int[] toppingCosts, int index, int currCost, int target) {
+		int diff = Math.abs(currCost - target);
+		if (diff < minDiff || (diff == minDiff && currCost < closestCost)) {
+			minDiff = diff;
+			closestCost = currCost;
+		}
+		if (currCost == target) {
+			return;
+		}
+		if (index >= toppingCosts.length || currCost > target + minDiff) {
+			return;
+		}
+
+		for (int count = 0; count < 3; count++) {
+			dfs(toppingCosts, index + 1, currCost + toppingCosts[index] * count, target);
+		}
+	}
+
+	public static void main(String[] args) {
+		Leet_1774 sol = new Leet_1774();
+		int i = sol.closestCost(new int[] {1, 7}, new int[] {3, 4}, 10);
+		System.out.println(i);
+	}
+}
