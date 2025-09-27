1084302bf30af66d2dacbf5dff9d3e2747eaf6c2
chan99k <kjkj5868@gmail.com>
2025-09-27 17:02:10 +0900

    chore: Kotlin JVM plugin 및 Gradle toolchain resolver configuration 추가

--- algorithm/build.gradle.kts
+++ algorithm/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    kotlin("jvm") version "2.2.0"
 }
 
 group = "chan99k"
@@ -13,8 +14,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.25.3")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
 }
