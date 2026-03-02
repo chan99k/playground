dd8e9f531f369868dad3772b9fa9498e697b7571
chan99k <kjkj5868@gmail.com>
2026-03-02 16:17:27 +0900

    chore: JVM target 불일치 → Kotlin 모듈에 jvmToolchain(24) 추가하여 해결

--- 00-algorithm/solve-kotlin/build.gradle.kts
+++ 00-algorithm/solve-kotlin/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
+kotlin {
+    jvmToolchain(24)
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
